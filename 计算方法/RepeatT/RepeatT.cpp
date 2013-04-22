fce4f671c5dfb5e440b23cfd4483cd5ad8602f78
孔德飞 <kdf5000@163.com>
2013-04-22 13:12:12 +0800

    变步长梯形求积
    改进梯形求积，通过改变步长求解积分

--- 计算方法/RepeatT/RepeatT.cpp
+++ 计算方法/RepeatT/RepeatT.cpp
@@ -1,5 +1,5 @@
 /*
-*功能：变步长求积公式
+*功能：变步长梯形求积公式
 */
 #include <stdio.h>
 #include <math.h>
@@ -8,10 +8,12 @@
 double fun(double x);
 //变步长梯形积分
 double RepeatT(double a,double b,int n);
+//变步长求积公式
+double changeRepeatT(double a,double b,double E);
 
 int main()
 {
-	printf("sin(x*x) 在区间(0,3.14)上的积分为：%lf\n",RepeatT(0.0,3.141592653,100));
+	printf("e^(-x*x) 在区间(0,1)上的积分为：%lf\n",changeRepeatT(0.0,1.0,0.00001));
 	return 0;
 }
 
@@ -28,5 +30,28 @@ double RepeatT(double a,double b,int n)
 //积分函数
 double fun(double x)
 {
-	return (sin(x*x));
+	return (exp(-x*x));
+}
+
+//变步长求积公式
+double changeRepeatT(double a,double b,double E)
+{
+	int n=1; //n等分
+	double h; //步长
+	int k=0;
+	int i;		//控制循环变量
+	double sumT0 = 0;  //记录每次积分值
+	double sumT1 = RepeatT(a,b,n);
+	printf("k=%d T=%lf\n",k,sumT1);
+
+	//变步长计算复化梯形积分
+	while(fabs(sumT0-sumT1)>E)
+	{
+		n *=2 ;
+		k++;
+		sumT0 = sumT1;
+		sumT1 = RepeatT(a,b,n);
+		printf("k=%d T=%lf\n",k,sumT1);
+	}
+	return sumT1;
 }
